1052e76c643ad1448a39b8e0028ec50c30da1d12
Ali Aslam <alikaslam@gmail.com>
2014-08-25 22:49:23 -0400

    Adds the Passport local strategy code

--- server/server.js
+++ server/server.js
@@ -4,14 +4,33 @@ var mongoose = require("mongoose");
 var bodyParser = require("body-parser");
 var cors = require("cors");
 var passport = require("passport");
+var LocalStrategy = require('passport-local').Strategy;
 var morgan = require("morgan");
 var app = express();
 
 
 // require('./config/passport')(passport); // pass passport for configuration
 
+// Use the LocalStrategy within Passport.
+//   Strategies in passport require a `verify` function, which accept
+//   credentials (in this case, a username and password), and invoke a callback
+//   with a user object.  This would query a database;
+passport.use(new LocalStrategy(function(username, password, done) {
+    User.findOne({ username: username }, function(err, user) {
+        if (err) { return done(err); }
+        if (!user) { return done(null, false, { message: 'Unknown user ' + username }); }
+        user.comparePassword(password, function(err, isMatch) {
+            if (err) return done(err);
+            if(isMatch) {
+                return done(null, user);
+            } else {
+                return done(null, false, { message: 'Invalid password' });
+            }
+        });
+    });
+}));
 
-
+app.use(passport.initialize());
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
